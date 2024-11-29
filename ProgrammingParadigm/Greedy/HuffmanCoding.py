e03354967643d50eeb7a3cb60b879088dde5249b
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2024-11-29 06:08:07 -0800

    Create HuffmanCoding.py

---
+++ ProgrammingParadigm/Greedy/HuffmanCoding.py
@@ -0,0 +1,17 @@
+# def huffmanCoding(c):
+#   n=len(c)
+#   Q=c
+#   for i in len(1,n-1):
+#     temp=
+#code reference from Geek
+import heapq
+class node:
+  def _init_(self,freq,left=None,right,None,symbol):
+    self.freq = freq
+    self.symbol
+    self.left=left
+    self.right=right
+    self.huffman=''
+  def _lt_(self,next):
+    return self.freq<next.freq
+    
